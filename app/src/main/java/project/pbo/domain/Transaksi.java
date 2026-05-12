1a043f12622076335d0520497bdb127d1107a731
xvasgit <erlanggapermana8279@gmail.com>
2026-05-12 23:49:05 +0700

    fixed nama-nama field

--- app/src/main/java/project/pbo/domain/Transaksi.java
+++ app/src/main/java/project/pbo/domain/Transaksi.java
@@ -1,6 +1,6 @@
 package project.pbo.domain;
 public class Transaksi {     
-    //note adit : tolong di class ini akan ada field (variabel) (String)idTransaksi, (String)nikPelanggan, (String)platNomor, (urasiHari, (otalBayar, dan (tatus
+    //note adit : tolong di class ini akan ada field (variabel) (String)idTransaksi, (String)nikPelanggan, (String)platNomor, (int)durasiHari, (double)totalBayar, dan (String)status
     //            sebaiknya nama field ini sesuai dengan note di atas agar menyamakan dan memudahkan di kebutuhan file .json
 
 
